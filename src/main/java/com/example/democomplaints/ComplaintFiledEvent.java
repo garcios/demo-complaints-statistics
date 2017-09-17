6ee3bc1f6c0150610e9e8db1b1891590664c0ddc
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2017-09-17 14:18:26 +0800

    refactoctored codes

--- src/main/java/com/example/democomplaints/ComplaintFiledEvent.java
+++ src/main/java/com/example/democomplaints/ComplaintFiledEvent.java
@@ -1,4 +1,4 @@
-package com.example.event;
+package com.example.democomplaints;
 
 public class ComplaintFiledEvent {
 
--- src/main/java/com/example/democomplaintsstatistics/StatisticsAPI.java
+++ src/main/java/com/example/democomplaintsstatistics/StatisticsAPI.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.event.ComplaintFiledEvent;
+import com.example.democomplaints.ComplaintFiledEvent;
 
 @ProcessingGroup("statistics")
 @RestController
